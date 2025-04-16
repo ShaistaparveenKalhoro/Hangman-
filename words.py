cc870360a6e686fd514b6cdafb342e9c2d909b54
ShaistaparveenKalhoro <162023593+ShaistaparveenKalhoro@users.noreply.github.com>
2025-04-16 12:33:13 +0500

    Add files via upload

---
+++ words.py
@@ -0,0 +1,12 @@
+words = [
+    'python', 'programming', 'computer', 'algorithm', 'database',
+    'network', 'software', 'hardware', 'internet', 'website',
+    'application', 'developer', 'coding', 'testing', 'debugging',
+    'function', 'variable', 'class', 'object', 'method',
+    'loop', 'condition', 'statement', 'expression', 'operator',
+    'string', 'integer', 'float', 'boolean', 'array',
+    'dictionary', 'list', 'tuple', 'set', 'module',
+    'package', 'library', 'framework', 'api', 'interface',
+    'server', 'client', 'browser', 'database', 'query',
+    'algorithm', 'sorting', 'searching', 'recursion', 'iteration'
+] 
